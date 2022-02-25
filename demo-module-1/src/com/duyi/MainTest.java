8a371599b5bb6345769f46c510d705904c295640
yaojie3698 <100276174+yaojie3698@users.noreply.github.com>
2022-02-25 18:25:31 +0800

    Add files via upload

---
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -0,0 +1,9 @@
+package com.duyi;
+
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+        System.out.println("test change");
+    }
+}
